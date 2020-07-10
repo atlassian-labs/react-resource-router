ebe34cdaf2df53f2245ed4b6536e01d4f31e7a11
Jack Brown <jbrown2@atlassian.com>
2020-07-10 12:54:14 +1000

    add router hooks

--- src/controllers/hooks/router-store/index.tsx
+++ src/controllers/hooks/router-store/index.tsx
@@ -1,5 +1,13 @@
 import { useRouterStore, useRouterStoreStatic } from '../../router-store';
 import { RouterActionsType, RouterState } from '../../router-store/types';
+import {
+  BrowserHistory,
+  MatchParams,
+  Match,
+  Route,
+  Location,
+} from '../../../common/types';
+import matchPath from '../../../common/utils/match-route/matchPath';
 
 /**
  * Utility hook for accessing the public router store
@@ -17,4 +25,33 @@ export const useRouterActions = (): RouterActionsType => {
   const [, allActions] = useRouterStoreStatic();
 
   return allActions;
+};
+
+export const useLocation = (): Location => {
+  const [routerState] = useRouterStore();
+
+  return routerState.location;
+};
+
+export const useHistory = (): BrowserHistory => {
+  const [routerState] = useRouterStore();
+
+  return routerState.history;
+};
+
+export const useParams = (): MatchParams => {
+  const [routerState] = useRouterStore();
+
+  return routerState.match.params;
+};
+
+export const useRouteMatch = (route?: string | Route): Match => {
+  const [routerState] = useRouterStore();
+
+  return route
+    ? matchPath(
+        routerState.location.pathname,
+        typeof route === 'string' ? { path: route } : route
+      )
+    : routerState.match;
 };
