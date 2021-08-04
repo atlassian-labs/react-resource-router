2f8219426ce40cea70d09a0896119f02c52cca1f
prithveesh-atlassian <80444450+prithveesh-atlassian@users.noreply.github.com>
2021-08-04 10:01:54 +0530

    Implemented LRU Cache - Removing the least recently used resource (#87)

--- examples/routing-with-resources/home.tsx
+++ examples/routing-with-resources/home.tsx
@@ -4,10 +4,10 @@ import { aboutRoute } from './routes';
 
 export const homeResource = createResource({
   type: 'home',
-  getKey: () => 'data',
-  maxAge: 0,
+  getKey: () => 'breedList',
+  maxAge: 10000,
   getData: async () => {
-    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const response = await fetch('https://dog.ceo/api/breeds/list/all');
     const result: { message: string } = await response.json();
 
     return result;
@@ -17,18 +17,21 @@ export const homeResource = createResource({
 export const Home = () => {
   // eslint-disable-next-line
   const { data, loading, error } = useResource(homeResource);
+  const breeds = data?.message ? Object.keys(data.message) : [];
 
   return (
     <div>
-      <h1>Home</h1>
-      <Link to={aboutRoute} prefetch="hover">
-        Go to about
-      </Link>
+      <h1>Dog Breeds</h1>
       <section>
-        <p>A random picture of a cute dog</p>
-        <section>
-          {!loading && <img src={data?.message} alt="A cute dog!" />}
-        </section>
+        <ul>
+          {breeds.slice(0, 25).map(breed => (
+            <li key={breed}>
+              <Link to={aboutRoute} query={{ name: breed }}>
+                {breed}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </section>
     </div>
   );
