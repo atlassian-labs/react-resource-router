9cb00ab20073d1269456590e0e13a4b7d3d3d126
stas <sgolovenko@atlassian.com>
2023-04-21 12:03:14 +1000

    fix types

--- src/controllers/plugins/test.ts
+++ src/controllers/plugins/test.ts
@@ -6,13 +6,16 @@ describe('invokePluginLoad', () => {
   it('calls each plugin load method', () => {
     // (findRouterContext as any).mockReturnValue(firstContextMock);
     const pluginOne = {
+      id: '1',
       routeLoad: jest.fn(),
     };
     const pluginTwo = {
+      id: '2',
       routeLoad: jest.fn(),
     };
 
     const pluginThree = {
+      id: '3',
       routeLoad: jest.fn(),
     };
 
