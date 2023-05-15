77ad7159a4740693f08a23da832cdf11c0855bbc
stas <sgolovenko@atlassian.com>
2023-05-15 16:12:26 +1000

    Route shouldReload API

--- src/common/utils/should-reload-when-route-match-changes/index.ts
+++ src/common/utils/should-reload-when-route-match-changes/index.ts
@@ -5,25 +5,12 @@ type Options = {
   params?: string[];
 };
 export const shouldReloadWhenRouteMatchChanges =
-  (options: Options) =>
+  ({ params = [], query = [] }: Options) =>
   ({ context, prevContext }: Parameters<ShouldReloadFunction>[0]) => {
-    if (
-      options?.query !== undefined &&
-      options?.query.some(
-        q => context.match.query[q] !== prevContext.match.query[q]
-      )
-    ) {
-      return true;
-    }
-
-    if (
-      options?.params !== undefined &&
-      options?.params.some(
+    return (
+      params.some(
         p => context.match.params[p] !== prevContext.match.params[p]
-      )
-    ) {
-      return true;
-    }
-
-    return false;
+      ) ||
+      query.some(q => context.match.query[q] !== prevContext.match.query[q])
+    );
   };
